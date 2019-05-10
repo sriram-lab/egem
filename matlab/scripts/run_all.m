b23425e8779e911406207a26b97b280135baf276
ScottCampit <scampit@umich.edu>
2019-05-10 11:41:39 -0400

    changed run_all.m

--- matlab/scripts/run_all.m
+++ matlab/scripts/run_all.m
@@ -3,13 +3,13 @@ initCobraToolbox;
 changeCobraSolver('gurobi');
 
 % load different models
-load model % eGEM model
+load ./../models/model.mat % minimal eGEM model - need to 
 
-load recon1
-model = metabolicmodel;
+%load recon1
+%model = metabolicmodel;
 
-load supplementary_software_code acetylation_model
-model = acetylation_model; %Shen et al., 2019
+%load supplementary_software_code acetylation_model
+%model = acetylation_model; %Shen et al., 2019
 
 %% Correlation values between histone markers and metabolic flux
 %histone_corr(model, 'amet', [], 'n', 1, 1E-2, 1, 1E-3, 0);
@@ -18,27 +18,51 @@ model = acetylation_model; %Shen et al., 2019
 %% Heatmap of metabolic reactions vs excess/depletion of medium coponents
 
 % Use params for testing 
-compartment = 'n';
-epsilon2 = 1E-3;
-scaling = [];
+%compartment = 'n';
+%epsilon2 = 1E-3;
+%scaling = [];
 
-[excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
-    excess_shadow, depletion_shadow] = make_heatmap(model, 'n',...
-    epsilon2, []);
+%[excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
+%    excess_shadow, depletion_shadow] = make_heatmap(model, 'n',...
+%    epsilon2, []);
+
+var = {'./../var/metabolites.mat', './../var/cellmedia.mat', './../var/mediareactions1.mat'};
+for kk = 1:numel(var)
+    load(var{kk})
+end
 
 % input filename for saving
-filename = 
+filename = './../tables/eGEM.xlsx';
+colname = metabolites(:,3)';
+rowname = mediareactions1(:,2);
 
 epsilon2 = [1E-4, 1E-3, 1E-2, 0.1, 1, 1E4];
 for n=1:length(epsilon2)
     [excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
     excess_shadow, depletion_shadow] = make_heatmap(model, 'n',...
-    epsilon2, []);
-    %filename = 'acetylationmodel-nucleus.xlsx';
-    %xlswrite(filename, excess_flux, string(epsilon2(n)), 'A1:S50');
-    %xlswrite(filename, depletion_flux, string(epsilon2(n)), 'V1:AR50');
-    %xlswrite(filename, excess_redcost, string(epsilon2(n)), 'A52:S102');
-    %xlswrite(filename, depletion_redcost, string(epsilon2(n)), 'V52:AR102');
-    %xlswrite(filename, excess_shadow, string(epsilon2(n)), 'A104:S154');
-    %xlswrite(filename, depletion_shadow, string(epsilon2(n)), 'V104:AR154');
+    epsilon2(n), []);
+    % Excess flux
+    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A2:A51');
+    xlswrite(filename, excess_flux, string(epsilon2(n)), 'B2:U51');
+    % Depleted flux
+    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A2:A51');
+    xlswrite(filename, depletion_flux, string(epsilon2(n)), 'X2:AQ51');
+    % Excess reduced cost
+    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A54:A103');
+    xlswrite(filename, excess_redcost, string(epsilon2(n)), 'B54:U103');
+    % Depleted reduced cost
+    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A54:A103');
+    xlswrite(filename, depletion_redcost, string(epsilon2(n)), 'X54:AQ103');
+    % Excess shadow price
+    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A106:A155');
+    xlswrite(filename, excess_shadow, string(epsilon2(n)), 'B106:U155');
+    % Depleted shadow price
+    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A106:A155');
+    xlswrite(filename, depletion_shadow, string(epsilon2(n)), 'X106:AQ155');
 end
