92e3b0f1a37b51f08e7f10cec190a6ed642f90fc
Scott Campit <scampit@umich.edu>
2019-06-17 15:54:19 -0400

    First pull request (#4)
    * changed run_all.m
    
    * testing out git pull requests from scampit branch

--- matlab/scripts/run_all.m
+++ matlab/scripts/run_all.m
@@ -2,10 +2,17 @@
 initCobraToolbox;
 changeCobraSolver('gurobi');
 
+<<<<<<< HEAD
+% load different models
+load ./../models/model.mat % minimal eGEM model - need to 
+
+%load recon1
+=======
 load ./../models/eGEM.mat % minimal eGEM model 
 %load ./../models/acetyl2.mat % new acetylation model
 
 %load ./../models/recon1
+>>>>>>> ff015e3c4b46fc0ff3510c7840d25949dba527ce
 %model = metabolicmodel;
 
 %load supplementary_software_code acetylation_model
@@ -54,6 +61,52 @@ epsilon2 = epsilon2';
 %compartment = 'n';
 %epsilon2 = 1E-3;
 %scaling = [];
+<<<<<<< HEAD
+
+%[excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
+%    excess_shadow, depletion_shadow] = make_heatmap(model, 'n',...
+%    epsilon2, []);
+
+var = {'./../var/metabolites.mat', './../var/cellmedia.mat', './../var/mediareactions1.mat'};
+for kk = 1:numel(var)
+    load(var{kk})
+end
+
+% input filename for saving
+filename = './../tables/eGEM.xlsx';
+colname = metabolites(:,3)';
+rowname = mediareactions1(:,2);
+
+epsilon2 = [1E-4, 1E-3, 1E-2, 0.1, 1, 1E4];
+for n=1:length(epsilon2)
+    [excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
+    excess_shadow, depletion_shadow] = make_heatmap(model, 'n',...
+    epsilon2(n), []);
+    % Excess flux
+    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A2:A51');
+    xlswrite(filename, excess_flux, string(epsilon2(n)), 'B2:U51');
+    % Depleted flux
+    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A2:A51');
+    xlswrite(filename, depletion_flux, string(epsilon2(n)), 'X2:AQ51');
+    % Excess reduced cost
+    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A54:A103');
+    xlswrite(filename, excess_redcost, string(epsilon2(n)), 'B54:U103');
+    % Depleted reduced cost
+    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A54:A103');
+    xlswrite(filename, depletion_redcost, string(epsilon2(n)), 'X54:AQ103');
+    % Excess shadow price
+    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A106:A155');
+    xlswrite(filename, excess_shadow, string(epsilon2(n)), 'B106:U155');
+    % Depleted shadow price
+    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
+    xlswrite(filename, rowname, string(epsilon2(n)), 'A106:A155');
+    xlswrite(filename, depletion_shadow, string(epsilon2(n)), 'X106:AQ155');
+=======
 %[excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
 %    excess_shadow, depletion_shadow] = metabolic_sensitivity(model, 'n',...
 %    epsilon2, []);
@@ -101,4 +154,5 @@ for i=1:6
     surf(x(:,1,1), y(1,:,1), A(:,:,i));
     hold on;
     colorbar
+>>>>>>> ff015e3c4b46fc0ff3510c7840d25949dba527ce
 end
