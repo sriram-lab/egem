e9052bcfa9d94f02b706ea5d8021dcbc4d0acebd
ScottCampit <scampit@umich.edu>
2019-06-19 11:03:39 -0400

    Edit  to remove old code and have newest version of  work

--- matlab/scripts/run_all.m
+++ matlab/scripts/run_all.m
@@ -2,26 +2,31 @@
 initCobraToolbox;
 changeCobraSolver('gurobi');
 
-<<<<<<< HEAD
-% load different models
-load ./../models/model.mat % minimal eGEM model - need to 
+%% load different models
 
-%load recon1
-=======
-load ./../models/eGEM.mat % minimal eGEM model 
-%load ./../models/acetyl2.mat % new acetylation model
+% Minial eGEM -> does not contain other one carbon reactions
+%load ./../models/model.mat % minimal eGEM model - need to 
 
+% Human metabolic reconstruction 1
 %load ./../models/recon1
->>>>>>> ff015e3c4b46fc0ff3510c7840d25949dba527ce
 %model = metabolicmodel;
 
+% Current eGEM: This is also a minimal eGEM but does contain additional
+% reactions
+load ./../models/eGEM.mat % minimal eGEM model 
+
+% New acetylation metabolic model. Contains PDC and ACSS2.
+%load ./../models/acetyl2.mat % new acetylation model
+
+% Old acetylation metabolic model
 %load supplementary_software_code acetylation_model
 %model = acetylation_model; %Shen et al., 2019
 
 %% Correlation values between histone markers and metabolic flux
-% h3marks: list of H3 marks from CCLE data (column values)
-% h3names: list of CCLE cell lines (row values)
-% h3vals: matrix containing values corresponding to h3marks and h3names
+% INPUTS:
+    % h3marks: list of H3 marks from CCLE data (column values)
+    % h3names: list of CCLE cell lines (row values)
+    % h3vals: matrix containing values corresponding to h3marks and h3names
 
 % Use params for testing
 % epsilon2 = 1E-3
@@ -37,9 +42,13 @@ minfluxflag = 0;
 % Run with multiple objective coefficients to obtain dynamic range of
 % values
 %epsilon2 = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 0.1, 1];
+%compartment = ['n', 'c', 'm'];
 %for n = 1:length(epsilon2)
-%    [correl, pval] = histone_corr(model, compartment,...
-%        mode, epsilon, epsilon2(n), rho, kappa, minfluxflag);
+    %for m = 1:length(compartment)
+    %[excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
+    %excess_shadow, depletion_shadow] = metabolic_sensitivity(model, compartment,...
+    %epsilon2(n), scaling, 'single-reaction-analysis')
+    %end
 %end
 
 % Run with the optimized objective coefficients
@@ -54,68 +63,39 @@ epsilon2 = epsilon2';
 [correl, pval] = histone_corr(model, compartment,...
         mode, epsilon, epsilon2(n), rho, kappa, minfluxflag);
 
-
-%% Heatmap of metabolic reactions vs excess/depletion of medium coponents
+%% Metabolic sensitivity analysis for excess and depleted medium components
+    % INPUT:
+        % switch case arguments:
+            % single-reaction-analysis - optimizes the flux of a single
+            % reaction
+            % dyn - optimizes the flux of several reactions using the results
+            % from `single-reaction-analysis`
+            % grate - fix biomass to max value and optimize for histone
+            % markers
+    % OUTPUT: 
+        % Heatmaps of the metabolic fluxes, shadow prices, and reduced
+        % costs corresponding to each reaction / medium component pair.
 
 % Use params for testing 
-%compartment = 'n';
-%epsilon2 = 1E-3;
-%scaling = [];
-<<<<<<< HEAD
-
-%[excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
-%    excess_shadow, depletion_shadow] = make_heatmap(model, 'n',...
-%    epsilon2, []);
+compartment = 'n';
+epsilon2 = 1E-3;
+scaling = [];
 
 var = {'./../var/metabolites.mat', './../var/cellmedia.mat', './../var/mediareactions1.mat'};
 for kk = 1:numel(var)
     load(var{kk})
 end
 
-% input filename for saving
+% Input filename for saving --> !Add this as argument to function later!
 filename = './../tables/eGEM.xlsx';
 colname = metabolites(:,3)';
 rowname = mediareactions1(:,2);
 
-epsilon2 = [1E-4, 1E-3, 1E-2, 0.1, 1, 1E4];
-for n=1:length(epsilon2)
-    [excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
-    excess_shadow, depletion_shadow] = make_heatmap(model, 'n',...
-    epsilon2(n), []);
-    % Excess flux
-    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
-    xlswrite(filename, rowname, string(epsilon2(n)), 'A2:A51');
-    xlswrite(filename, excess_flux, string(epsilon2(n)), 'B2:U51');
-    % Depleted flux
-    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
-    xlswrite(filename, rowname, string(epsilon2(n)), 'A2:A51');
-    xlswrite(filename, depletion_flux, string(epsilon2(n)), 'X2:AQ51');
-    % Excess reduced cost
-    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
-    xlswrite(filename, rowname, string(epsilon2(n)), 'A54:A103');
-    xlswrite(filename, excess_redcost, string(epsilon2(n)), 'B54:U103');
-    % Depleted reduced cost
-    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
-    xlswrite(filename, rowname, string(epsilon2(n)), 'A54:A103');
-    xlswrite(filename, depletion_redcost, string(epsilon2(n)), 'X54:AQ103');
-    % Excess shadow price
-    xlswrite(filename, colname, string(epsilon2(n)), 'B1:U1');
-    xlswrite(filename, rowname, string(epsilon2(n)), 'A106:A155');
-    xlswrite(filename, excess_shadow, string(epsilon2(n)), 'B106:U155');
-    % Depleted shadow price
-    xlswrite(filename, colname, string(epsilon2(n)), 'X1:AQ1');
-    xlswrite(filename, rowname, string(epsilon2(n)), 'A106:A155');
-    xlswrite(filename, depletion_shadow, string(epsilon2(n)), 'X106:AQ155');
-=======
-%[excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
-%    excess_shadow, depletion_shadow] = metabolic_sensitivity(model, 'n',...
-%    epsilon2, []);
-
-% Sample some epsilon values to determine largest dynamic range
-%epsilon2 = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 0.1, 1];
+% Metabolic sensitivity analysis for optimized epsilon values obtained from
+% single optimization steps:
 
 % Use epsilon values that gave the largest dynamic range in metabolic
-% fluxes 
+% fluxes. !This was manually done, but should also be codified at some point! 
 epsilon2_excess = [1E-6, 1E-6, 1E-5, 1E-5, 1E-5, 1E-6, 1E-6, 1E-6, 1E-6,...
     1E-5, 1E-6, 1, 1E-6, 1E-6, 1, 1E-6, 1E-6, 1E-6, 1E-6, 1E-5];
 
@@ -125,23 +105,7 @@ epsilon2_depletion = [1E-6, 1E-5, 1E-5, 1E-5, 1E-5, 1E-5, 1E-6, 1E-6, 1E-5,...
 epsilon2 = [epsilon2_excess; epsilon2_depletion];
 epsilon2 = epsilon2';
 
-% Run metabolic_sensitivity
-    % for the switch case:
-        % dyn - maximize using dynamic range of metabolic fluxes
-        % grate - fix biomass to max value and optimize for histone
-        % markers
-
-% For single epsilon determination
-%compartment = ['n', 'c', 'm'];
-%for n = 1:length(epsilon2)
-    %for m = 1:length(compartment)
-%    [excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
-%    excess_shadow, depletion_shadow] = metabolic_sensitivity(model, 'n',...
-%    epsilon2(:, n), [], 'dyn');
-    %end
-%end
-
-% For optimizing with multiple epsilon values:
+% For optimizing multiple reactions simultaneously:
 [excess_flux, depletion_flux, excess_redcost, depletion_redcost,...
     excess_shadow, depletion_shadow] = metabolic_sensitivity(model, 'n',...
     epsilon2, [], 'dyn');
@@ -154,5 +118,4 @@ for i=1:6
     surf(x(:,1,1), y(1,:,1), A(:,:,i));
     hold on;
     colorbar
->>>>>>> ff015e3c4b46fc0ff3510c7840d25949dba527ce
 end
