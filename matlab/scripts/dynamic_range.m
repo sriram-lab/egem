54766b11f5e1dfaf4bf7b3a806c0d0fc50f498cf
ScottCampit <scampit@umich.edu>
2019-06-28 16:21:05 -0400

    add all

--- matlab/scripts/dynamic_range.m
+++ matlab/scripts/dynamic_range.m
@@ -1,25 +1,41 @@
-function [epsilon2] = dynamic_range(arr1, arr2, arr3, arr4, arr5, arr6, eps2)
-% dynamic_range returns the epsilon values that will be used for FBA
-% and FVA in metabolic_sensitivity.
+function [epsilon2] = dynamic_range(eps1, eps2, eps3, eps4, eps5, eps6, flag)
 
-% INPUT:
-    % arr1 to arr6: arrays containing values corresponding to fixed epsilon values
-    % eps2: an array of epsilon values from 1E-6 to 1
+%% dynamic_range
+    % Function returns the epsilon values that will be used for FBA
+    % and FVA in metabolic_sensitivity.
+
+    % INPUT:
+        % eps1 to eps6: arrays containing values corresponding to fixed epsilon values
+        % eps2: an array of epsilon values from 1E-6 to 1
+
+    % OUTPUT:
+        % epsilon2: an array that contains the epsilon values for
+        % simultaneous optimization of metabolic model
+
+if flag == 'excess'
+    for i=1:6
+        eval(["mx(i,:) = max(eps" i ".excess_flux);"]); 
+        eval(["mn(i,:) = min(eps" i ".excess_flux);"]);
+        dif(i,:) = mx(i,:) - mn(i,:);
+    end
     
-% OUTPUT:
-    % epsilon2: an array that contains the epsilon values for
-    % simultaneous optimization of metabolic model
-
-% For each array, get the dynamic range, stored in `dif` variable
-for i=1:6
-    eval(["mx(i,:) = max(arr" i ");"]); %(1x20) array
-    eval(["mn(i,:) = min(arr" i ");"]);
-    dif(i,:) = mx(i,:) - mn(i,:);
-end
-
-% Get the epsilon2 that produced the highest dynamic range
-[~, idx] = max(dif);
-for i=1:length(idx)
-    epsilon2(i,1) = eps2(idx(1,i));
+    % Get the epsilon2 that produced the highest dynamic range
+    [~, idx] = max(dif);
+    for i=1:length(idx)
+        epsilon2(i,1) = eps2(idx(1,i));
+    end
+    
+elseif flag == 'depletion'
+    for i=1:6
+        eval(["mx(i,:) = max(eps" i ".depletion_flux);"]);
+        eval(["mn(i,:) = min(eps" i ".depletion_flux);"]);
+        dif(i,:) = mx(i,:) - mn(i,:);
+    end
+    
+    % Get the epsilon2 that produced the highest dynamic range
+    [~, idx] = max(dif);
+    for i=1:length(idx)
+        epsilon2(i,1) = eps2(idx(1,i));
+    end
 end
 end
