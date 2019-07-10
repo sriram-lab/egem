7c36c7d66241ae1075658e6e9995a91fa1096c64
Scott Campit <scampit@umich.edu>
2019-07-10 10:31:50 -0400

    add

--- matlab/scripts/dynamic_range.m
+++ matlab/scripts/dynamic_range.m
@@ -1,25 +1,96 @@
-function [epsilon2] = dynamic_range(arr1, arr2, arr3, arr4, arr5, arr6, eps2)
-% dynamic_range returns the epsilon values that will be used for FBA
-% and FVA in metabolic_sensitivity.
+function epsilon2 = dynamic_range(eps1, eps2, eps3, eps4, eps5, eps6, eps7, type)
+%% dynamic_range
+    % Function returns the epsilon values that will be used for FBA
+    % and FVA in metabolic_sensitivity.
 
-% INPUT:
-    % arr1 to arr6: arrays containing values corresponding to fixed epsilon values
-    % eps2: an array of epsilon values from 1E-6 to 1
-    
-% OUTPUT:
-    % epsilon2: an array that contains the epsilon values for
-    % simultaneous optimization of metabolic model
+    % INPUT:
+        % eps1 to eps6: arrays containing values corresponding to fixed epsilon values
+        % eps2: an array of epsilon values from 1E-6 to 1
 
-% For each array, get the dynamic range, stored in `dif` variable
-for i=1:6
-    eval(["mx(i,:) = max(arr" i ");"]); %(1x20) array
-    eval(["mn(i,:) = min(arr" i ");"]);
-    dif(i,:) = mx(i,:) - mn(i,:);
-end
+    % OUTPUT:
+        % epsilon2: an array that contains the epsilon values for
+        % simultaneous optimization of metabolic model
+switch type
+    case 'dynamic'
+        % Dynamic range for metabolic flux in excess medium conditions
+        for i=1:7
+            eval(strcat("mx(i,:) = max(eps", string(i), ".excess_flux);")); 
+            eval(strcat("mn(i,:) = min(eps", string(i), ".excess_flux);"));
+            dif(i,:) = mx(i,:) - mn(i,:);
+        end
 
-% Get the epsilon2 that produced the highest dynamic range
-[~, idx] = max(dif);
-for i=1:length(idx)
-    epsilon2(i,1) = eps2(idx(1,i));
+        [~, idx] = max(dif);
+        fill_val = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 1E-1, 1];
+        for i=1:length(idx)
+            excess_epsilon2(i,1) = fill_val(idx(1,i));
+        end
+
+        % Dynamic range for metabolic flux in depleted medium conditions
+        for i=1:7
+            eval(strcat("mx(i,:) = max(eps", string(i), ".depletion_flux);")); 
+            eval(strcat("mn(i,:) = min(eps", string(i), ".depletion_flux);"));
+            dif(i,:) = mx(i,:) - mn(i,:);
+        end
+
+        [~, idx] = max(dif);
+        fill_val = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 1E-1, 1];
+        for i=1:length(idx)
+            depletion_epsilon2(i,1) = fill_val(idx(1,i));
+        end
+
+        % Concatenate
+        epsilon2 = [excess_epsilon2, depletion_epsilon2];
+        
+    case 'max'
+        % Dynamic range for metabolic flux in excess medium conditions
+        for i=1:7
+            eval(strcat("mx(i,:) = max(eps", string(i), ".excess_flux);")); 
+        end
+
+        [~, idx] = max(mx);
+        fill_val = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 1E-1, 1];
+        for i=1:length(idx)
+            excess_epsilon2(i,1) = fill_val(idx(1,i));
+        end
+        
+        % Dynamic range for metabolic flux in depleted medium conditions
+        for i=1:7
+            eval(strcat("mx(i,:) = max(eps", string(i), ".depletion_flux);")); 
+        end
+
+        [~, idx] = max(mx);
+        fill_val = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 1E-1, 1];
+        for i=1:length(idx)
+            depletion_epsilon2(i,1) = fill_val(idx(1,i));
+        end
+        
+        % Concatenate
+        epsilon2 = [excess_epsilon2, depletion_epsilon2];
+        
+    case 'min'
+        % Dynamic range for metabolic flux in excess medium conditions
+        for i=1:7
+            eval(strcat("mn(i,:) = min(eps", string(i), ".excess_flux);"));
+        end
+
+        [~, idx] = max(mn);
+        fill_val = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 1E-1, 1];
+        for i=1:length(idx)
+            excess_epsilon2(i,1) = fill_val(idx(1,i));
+        end
+        
+        % Dynamic range for metabolic flux in depleted medium conditions
+        for i=1:7
+            eval(strcat("mn(i,:) = min(eps", string(i), ".depletion_flux);"));
+        end
+
+        [~, idx] = max(mn);
+        fill_val = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 1E-1, 1];
+        for i=1:length(idx)
+            depletion_epsilon2(i,1) = fill_val(idx(1,i));
+        end
+        
+        % Concatenate
+        epsilon2 = [excess_epsilon2, depletion_epsilon2];
 end
 end
--- matlab/scripts/run_all.m
+++ matlab/scripts/run_all.m
@@ -2,18 +2,16 @@
 initCobraToolbox;
 changeCobraSolver('gurobi');
 %% load models. Must run from "scripts" directory
-load ./../models/eGEM.mat % minimal eGEM model 
-%load ./../models/acetyl2.mat % new acetylation model
+load ./../models/eGEM.mat % bulk eGEM model 
+model = eGEM;
 
+%load ./../models/acetyl2.mat % new acetylation model
 % Minial eGEM -> does not contain other one carbon reactions
-load ./../models/min.mat % minimal eGEM model
+%load ./../models/min.mat % minimal eGEM model
 %model = eGEM_min;
 % Human metabolic reconstruction 1
 %load ./../models/recon1
 %model = metabolicmodel;
-
-% Current eGEM
-%load ./../models/eGEM.mat
 
 % New acetylation metabolic model. Contains PDC and ACSS2.
 %load ./../models/acetyl2.mat % new acetylation model
@@ -21,40 +19,8 @@ load ./../models/min.mat % minimal eGEM model
 % Old acetylation metabolic model
 %load supplementary_software_code acetylation_model
 %model = acetylation_model; %Shen et al., 2019
-model = acetylation_model;
+%model = acetylation_model;
 
-%% Correlation values between histone markers and metabolic flux
-% INPUTS:
-    % h3marks: list of H3 marks from CCLE data (column values)
-    % h3names: list of CCLE cell lines (row values)
-    % h3vals: matrix containing values corresponding to h3marks and h3names
-
-% Use params for testing
-% epsilon2 = 1E-3
-
-% Initialize params for iMAT algorithm
-compartment = 'n';
-mode = 1;
-epsilon = 1E-3;
-rho = 1;
-kappa = 1E-3;
-minfluxflag = 0;
-
-% Run with multiple objective coefficients to obtain dynamic range of
-% values
-%epsilon2 = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 0.1, 1];
-
-% Run with the optimized objective coefficients
-epsilon2_excess = [1E-6, 1E-6, 1E-5, 1E-5, 1E-5, 1E-6, 1E-6, 1E-6, 1E-6,...
-    1E-5, 1E-6, 1, 1E-6, 1E-6, 1, 1E-6, 1E-6, 1E-6, 1E-6, 1E-5];
-
-epsilon2_depletion = [1E-6, 1E-5, 1E-5, 1E-5, 1E-5, 1E-5, 1E-6, 1E-6, 1E-5,...
-    1E-6, 1E-4, 1, 1E-5, 1E-6, 1, 1E-6, 1E-6, 1E-6, 1E-6, 1E-6];
-
-epsilon2 = [epsilon2_excess; epsilon2_depletion];
-epsilon2 = epsilon2';
-[correl, pval] = histone_corr(model, compartment,...
-        mode, epsilon, epsilon2(n), rho, kappa, minfluxflag);
 
 %% Metabolic sensitivity analysis for excess and depleted medium components
     % INPUT:
@@ -69,66 +35,69 @@ epsilon2 = epsilon2';
         % Heatmaps of the metabolic fluxes, shadow prices, and reduced
         % costs corresponding to each reaction / medium component pair.
 
-% Use params for testing 
-compartment = 'n';
-epsilon2 = 1E-3;
-scaling = [];
-
-var = {'./../vars/metabolites.mat', './../vars/cellmedia.mat', './../vars/mediareactions1.mat'};
-for kk = 1:numel(var)
-    load(var{kk})
-end
-
-% Input filename for saving --> !Add this as argument to function later!
-filename = './../tables/eGEM.xlsx';
-colname = metabolites(:,3)';
-rowname = mediareactions1(:,2);
-
-% Metabolic sensitivity analysis for optimized epsilon values obtained from
-% single optimization steps:
-
-% Single test case for epsilon
-epsilon2 = 1E-3;
-exp = 'single-reaction-analysis';
-
+% Optimization 1: Run Single reaction activity (SRA)
+medium_of_interest = {'RPMI', 'DMEM', 'L15'};
 epsilon2 = [1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 0.1, 1];
-%compartment = ['n', 'c', 'm'];
-%for n = 1:length(epsilon2)
-    %for m = 1:length(compartment)
-[sra, ~, ~, ~, ~] = metabolic_sensitivity(min, 'n', 1E-6, 'zscore', 'sra',...
-    'RPMI', []);
-    %end
-%end
-
-% Use epsilon values that gave the largest dynamic range in metabolic
-% fluxes. !This was manually done, but should also be codified at some point! 
-epsilon2_excess = [1E-6, 1E-6, 1E-5, 1E-5, 1E-5, 1E-6, 1E-6, 1E-6, 1E-6,...
-    1E-5, 1E-6, 1, 1E-6, 1E-6, 1, 1E-6, 1E-6, 1E-6, 1E-6, 1E-5];
-
-epsilon2_depletion = [1E-6, 1E-5, 1E-5, 1E-5, 1E-5, 1E-5, 1E-6, 1E-6, 1E-5,...
-    1E-6, 1E-4, 1, 1E-5, 1E-6, 1, 1E-6, 1E-6, 1E-6, 1E-6, 1E-6];
-
-epsilon2 = [epsilon2_excess; epsilon2_depletion];
-epsilon2 = epsilon2';
-
-[~, fba, ~, excess_soln, depletion_soln] = metabolic_sensitivity(min, 'n',...
-        epsilon2, 'zscore', 'fba', 'RPMI');
-
-% For optimizing multiple reactions simultaneously using FBA with different
-% media:
-[~, media] = xlsfinfo('./../../data/uptake.xlsx');
-for fil=1:length(sheets)
-    [~, fba, ~, excess_soln, depletion_soln] = metabolic_sensitivity(min, 'n',...
-        epsilon2, 'zscore', 'fba', media(fil));
+for med = 1:length(medium_of_interest)
+    disp(medium_of_interest(med))
+    for n = 1:length(epsilon2)
+        % Run all
+        str =  strcat("[sra", string(n), '_', medium_of_interest(med), ", ~, ~, ~, ~] = metabolic_sensitivity(model, 'n', epsilon2(n), 'zscore', 'sra', medium_of_interest(med), []);");
+        eval(str)
+        % Plot all
+        str = strcat("plot_heatmap(sra", string(n), '_', medium_of_interest(med), ",'sra', epsilon2(n), medium_of_interest(med))");
+        eval(str)
+    end
 end
 
-% For optimizing multiple reactions simultaneously using FVA with different
-% media:
-[~, media] = xlsfinfo('./../../data/uptake.xlsx');
-for fil=1:length(sheets)
-    [~, ~, fva, ~] = metabolic_sensitivity(min, 'n',...
-        epsilon2, 'zscore', 'fva', media(fil), 99);
+% Calculate epsilon2 values to use for fba by dynamic range
+epsilon2_dmem = dynamic_range(sra1_DMEM, sra2_DMEM, sra3_DMEM, sra4_DMEM,...
+    sra5_DMEM, sra6_DMEM, sra7_DMEM, "dynamic");
+epsilon2_rpmi = dynamic_range(sra1_RPMI, sra2_RPMI, sra3_RPMI, sra4_RPMI,...
+    sra5_RPMI, sra6_RPMI, sra7_RPMI, "dynamic");
+epsilon2_l15 = dynamic_range(sra1_L15, sra2_L15, sra3_L15, sra4_L15,...
+    sra5_L15, sra6_L15, sra7_L15, "dynamic");
+
+% Optimization 2: Run Flux balance analysis (FBA)
+medium_of_interest = {'RPMI', 'DMEM', 'L15'};
+for med = 1:length(medium_of_interest)
+    disp(medium_of_interest(med))
+    % Run all
+    str =  strcat("[~, fba_", lower(medium_of_interest(med)),", ~, ~, ~] =metabolic_sensitivity(model, 'n', epsilon2_", lower(medium_of_interest(med)), ", 'zscore', 'fba', medium_of_interest(med), []);");
+    eval(str)
+    % Plot all
+    str = strcat("plot_heatmap(fba_", lower(medium_of_interest(med)), ",'fba', epsilon2, medium_of_interest(med))");
+    eval(str)
 end
+
+% Optimization 3: Run Flux variability analysis (FVA)
+medium_of_interest = {'RPMI', 'DMEM', 'L15'};
+for med=1:length(medium_of_interest)
+    % Run all
+    str =  strcat("[~, ~,  fva_", lower(medium_of_interest(med)), " ~, ~] = metabolic_sensitivity(model, 'n', epsilon2_", lower(medium_of_interest(med)), ", 'zscore', 'fva', medium_of_interest(med), 99);");
+    eval(str)
+    % Plot all
+    str = strcat("plot_heatmap(fva_", lower(medium_of_interest(med)), ",'fva', epsilon2, medium_of_interest(med))");
+    %eval(str)
+end
+
+%% Correlation values between histone markers and metabolic flux
+% INPUTS:
+    % h3marks: list of H3 marks from CCLE data (column values)
+    % h3names: list of CCLE cell lines (row values)
+    % h3vals: matrix containing values corresponding to h3marks and h3names
+
+% Initialize params for iMAT algorithm
+compartment = 'n';
+mode = 1;
+epsilon = 1E-3;
+rho = 1;
+kappa = 1E-3;
+minfluxflag = 0;
+    
+[correl, pval] = histone_corr(model, compartment,...
+        mode, epsilon, epsilon2(n), rho, kappa, minfluxflag);
+    
 %% Density plot
 A = densityplot('eGEMn');
 
