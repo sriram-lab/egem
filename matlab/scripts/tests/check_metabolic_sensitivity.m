1dc09cfb11bdc123b1b878d4f0220abc389d66a8
Scott Campit <scampit@umich.edu>
2019-08-16 13:00:55 -0400

    Fixed DM reactions in eGEM

--- matlab/scripts/tests/check_metabolic_sensitivity.m
+++
@@ -1,8 +0,0 @@
-% Check metabolic_sensitivity module
-
-fileList = dir('./../metabolic_sensitivity');
-for file = 1:length(fileList)
-    disp(fileList(file))
-end
-    checkcode fileList(file)
-end
