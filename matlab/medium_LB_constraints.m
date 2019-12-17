51fa3244725badcc1963379fb01e1224b6d270ed
ScottCampit <scampit@umich.edu>
2019-12-17 16:29:11 -0500

    add all

--- matlab/medium_LB_constraints.m
+++ matlab/medium_LB_constraints.m
@@ -1,12 +1,12 @@
 function model = medium_LB_constraints(model, queried_medium)
 
-path = 'final_medium2.xlsx';
+file = 'final_medium2.xlsx';
 
 if verLessThan('matlab', '9.6.0.1072779')
-    [~, sheetNames] = xlsfinfo(path);
+    [~, sheetNames] = xlsfinfo(file);
     for sheets = 1:length(sheetNames)
         if ismember(string(sheetNames(sheets)), queried_medium)
-            [adjustedLB, rxn_ids] = xlsread(path, string(sheetNames{sheets}));
+            [adjustedLB, rxn_ids] = xlsread(file, string(sheetNames{sheets}));
             rxn_ids(1, :) = [];
             rxn_ids(:, 1) = [];
 
@@ -16,7 +16,7 @@ if verLessThan('matlab', '9.6.0.1072779')
             end
 
         elseif ismember({'nan'}, queried_medium)
-            [adjustedLB, rxn_ids] = xlsread(path, 'RPMI');
+            [adjustedLB, rxn_ids] = xlsread(file, 'RPMI');
             rxn_ids(1,:) = [];
             rxn_ids(:,1) = [];
 
@@ -29,10 +29,10 @@ if verLessThan('matlab', '9.6.0.1072779')
     end
         
 else
-    [~, sheetNames] = xlsfinfo(path);
+    [~, sheetNames] = xlsfinfo(file);
     for sheets = 1:length(sheetNames)
         if ismember(string(sheetNames(sheets)), string(queried_medium))
-            dataArray = readcell(path, 'Sheet', string(sheetNames(sheets)));
+            dataArray = readcell(file, 'Sheet', string(sheetNames(sheets)));
             dataArray(1,:) = [];
             dataArray(:,1) = [];
 
@@ -42,7 +42,7 @@ else
             end
 
         elseif ismember({'nan'}, queried_medium)
-            dataArray = readcell(path, 'Sheet', 'RPMI');
+            dataArray = readcell(file, 'Sheet', 'RPMI');
             dataArray(1,:) = [];
             dataArray(:,1) = [];
 
