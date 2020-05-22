9c915621dfc115f14f72e2842a9bfed4638f9f36
ScottCampit <scampit@umich.edu>
2020-05-22 16:29:16 -0400

    Add all modules from new analysis to integrate with old publication

--- matlab/saveData.m
+++
@@ -1,9 +0,0 @@
-function [dataStruct] = saveData(data, var, exp)
-    fileName = './../../vars/histoneCorrResults.mat';
-    varName = strcat(string(var), var, exp);
-    if exist(fileName, 'file')
-        save(fileName, varName, '-append')
-    else
-        save(fileName, varName)
-    end
-end
