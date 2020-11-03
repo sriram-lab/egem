bdbff7712967faf391659585a4bad4c6282fc2c9
Scott Campit <scampit@umich.edu>
2020-11-03 16:42:15 -0500

    Removed unnecessary files from srv/python/ml/
    Removed:
      * error.txt
      * nonlinear_regression.py
      * output.txt
      * regressor.py

--- srv/python/ml/slurm-13093003.sh
+++ srv/python/ml/slurm-13093003.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=nonlinear_regress1
+#SBATCH --job-name=met2gcp_regression
 #SBATCH --mail-user=scampit@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --output=./output.txt
-#SBATCH --error=./error.txt
+#SBATCH --output=./output.log
+#SBATCH --error=./error.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=1g
